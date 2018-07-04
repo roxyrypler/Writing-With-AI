43142b41224d31c7278e859cdc2018c00188dae6
roxyrypler <30288569+roxyrypler@users.noreply.github.com>
2018-07-04 23:29:21 +0200

    semi BrokenUpdate

--- sketch.js
+++ sketch.js
@@ -7,6 +7,9 @@ let textInput;
 let tempSlider;
 let lengthSlider;
 let modelLoader; 
+let createdDivNR = 0;
+let divNRArr = [];
+let gatheredText = [];
 
 function setup() {
   noCanvas();
@@ -19,9 +22,9 @@ function setup() {
   tempSlider = select('#tempSlider');
 
   // Run generate anytime something changes
-  textInput.input(generate);
-  lengthSlider.input(generate);
-  tempSlider.input(generate);
+  //textInput.input(generate);
+  lengthSlider.input(updateSliders);
+  tempSlider.input(updateSliders);
 }
 
 function startWriter() {
@@ -31,33 +34,54 @@ function startWriter() {
 	
 	if (modelSelector == 1) {
 		lstm = ml5.LSTMGenerator('models/bolano/', modelReady);
+		document.getElementById('basedOn').innerHTML = 'bolano';
 		//console.log(lstm);
 	}else if (modelSelector == 2) {
 		lstm = ml5.LSTMGenerator('models/charlotte_bronte/', modelReady);
+		document.getElementById('basedOn').innerHTML = 'charlotte bronte';
 		//console.log(lstm);
 	}else if (modelSelector == 3) {
 		lstm = ml5.LSTMGenerator('models/darwin/', modelReady);
+		document.getElementById('basedOn').innerHTML = 'darwin';
 		//console.log(lstm);
 	}else if (modelSelector == 4) {
 		lstm = ml5.LSTMGenerator('models/dubois/', modelReady);
+		document.getElementById('basedOn').innerHTML = 'dubois';
 		//console.log(lstm);
 	}else if (modelSelector == 5) {
 		lstm = ml5.LSTMGenerator('models/hemingway/', modelReady);
+		document.getElementById('basedOn').innerHTML = 'hemingway';
 		//console.log(lstm);
 	}else if (modelSelector == 6) {
 		lstm = ml5.LSTMGenerator('models/shakespeare/', modelReady);
+		document.getElementById('basedOn').innerHTML = 'shakespeare';
 		//console.log(lstm);
 	}else if (modelSelector == 7) {
 		lstm = ml5.LSTMGenerator('models/woolf/', modelReady);
+		document.getElementById('basedOn').innerHTML = 'woolf';
 		//console.log(lstm);
 	}else if (modelSelector == 8) {
 		lstm = ml5.LSTMGenerator('models/zora_neale_hurston/', modelReady);
+		document.getElementById('basedOn').innerHTML = 'zora neale hurston';
+		//console.log(lstm);
+	}else if (modelSelector == 9) {
+		lstm = ml5.LSTMGenerator('models/1984_By_George_Orwell/', modelReady);
+		document.getElementById('basedOn').innerHTML = '1984 By George Orwell';
 		//console.log(lstm);
 	}
 }
 
 function modelReady() {
   select('#status').html('Model Loaded');
+}
+
+function GenerateTextBTN() {
+	generate();
+}
+
+function updateSliders() {
+	select('#length').html(lengthSlider.value());
+  	select('#temperature').html(tempSlider.value());
 }
 
 
@@ -101,18 +125,21 @@ function generate() {
 }
 
 function savTextAndResetSeed() {
+	createdDivNR ++;
+	divNRArr.push('CreatedP' + createdDivNR);
 	
 	let getOriginal = document.getElementById('original').innerText;
 	let getPredicted = document.getElementById('prediction').innerText;
 	let combinedText = getOriginal + getPredicted;
 	
-	let createP = document.createElement('p');
+	let createP = document.createElement('textarea');
 	createP.id = 'CreatedP';
+	createP.className = 'form-control';
 	let textInP = document.createTextNode(combinedText);
 	createP.appendChild(textInP);
 	
 	let createDIV = document.createElement('div');
-	createDIV.id = 'createdDIV';
+	createDIV.id = 'createdDIV' + createdDivNR;
 	createDIV.appendChild(createP);
 	
 	let divParrent = document.getElementById('createdContent');
@@ -120,18 +147,21 @@ function savTextAndResetSeed() {
 }
 
 function saveAndReset() {
+	createdDivNR++;
+	divNRArr.push('CreatedP' + createdDivNR);
 	
 	let getOriginal = document.getElementById('original').innerText;
 	let getPredicted = document.getElementById('prediction').innerText;
 	let combinedText = getOriginal + getPredicted;
 	
-	let createP = document.createElement('p');
+	let createP = document.createElement('textarea');
 	createP.id = 'CreatedP';
+	createP.className = 'form-control';
 	let textInP = document.createTextNode(combinedText);
 	createP.appendChild(textInP);
 	
 	let createDIV = document.createElement('div');
-	createDIV.id = 'createdDIV';
+	createDIV.id = 'createdDIV' + createdDivNR;
 	createDIV.appendChild(createP);
 	
 	let divParrent = document.getElementById('createdContent');
@@ -140,6 +170,20 @@ function saveAndReset() {
 	document.getElementById('textValue').value = '';
 	getOriginal = document.getElementById('original').innerText = '';
 	getPredicted = document.getElementById('prediction').innerText = '';
+}
+
+function saveYourStory() {
+	
+	//let getAllText = document.getElementById('CreatedP').innerHTML;
+	
+	for (let i = 0; i < divNRArr.length; i++) {
+		let getAllText = document.getElementById('createdDIV' + createdDivNR).innerHTML;
+		gatheredText.unshift(getAllText);
+	}
+	
+	console.log(gatheredText);
+	
+	
 }
 
 
